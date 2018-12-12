a17bb426d8d3b2028d977c5274682f996cd54f67
Andreas Wilcox <andreas.wilcox@m-sys.se>
2018-12-12 23:57:29 +0100

    Added IT-Gymnasiet game jam workshop presentation

---
+++ Assets/IT-Gymnasiet Workshop/PhysicsMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PhysicsMove : MonoBehaviour
+{
+    public float Speed;
+	public float JumpForce;
+    public Transform FeetGround;
+
+    private Quaternion _standardRotation;
+	private Rigidbody _rigidbody;
+	private float _jumpDelay;
+	private float _jumpDelayIncrease = 0.5f;
+
+	public void Start ()
+	{
+	    _standardRotation = transform.rotation;
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	public void Update ()
+	{
+        var speed = Speed;
+
+		var isOnGround = Physics.Raycast(FeetGround.position + Vector3.up * 0.1f, Vector3.down, 0.3f, ~LayerMask.GetMask("TransformObject", "Player"));
+
+		if (_jumpDelay <= 0.001f && isOnGround && Input.GetButton("Jump"))
+	    {
+			_rigidbody.AddForce(Vector3.up*JumpForce, ForceMode.Impulse);
+		    _jumpDelay += _jumpDelayIncrease;
+	    }
+
+		_jumpDelay = Mathf.Max(_jumpDelay - Time.deltaTime, 0);
+
+        var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+	    if (movement.sqrMagnitude > 0.01f)
+	    {
+		    var isoRotation = Quaternion.Euler(0, -45, 0);
+		    _rigidbody.MovePosition(_rigidbody.position + isoRotation*movement*Time.deltaTime*speed);
+
+	        var newRotation = _standardRotation*isoRotation*Quaternion.LookRotation(-new Vector3(movement.x, 0, movement.z), Vector3.up);
+            _rigidbody.MoveRotation(Quaternion.Slerp(transform.rotation, newRotation, 0.2f));
+        }
+    }
+}
---
+++ Assets/IT-Gymnasiet Workshop/TransformToObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformToObject : MonoBehaviour
+{
+	public GameObject CurrentVisual;
+	private readonly List<Collider> _closeObjects = new List<Collider>();
+
+	public void Update()
+	{
+	    if (Input.GetButtonUp("Fire1"))
+	    {
+		    var closestDistance = float.MaxValue;
+		    Collider closestObject = null;
+		    foreach (var closeObject in _closeObjects)
+		    {
+			    var distance = Vector3.Distance(closeObject.transform.position, transform.position);
+
+				if (distance < closestDistance)
+			    {
+				    closestDistance = distance;
+				    closestObject = closeObject;
+			    }
+		    }
+
+		    if (closestObject != null)
+		    {
+				Destroy(CurrentVisual);
+			    var parentRigidbody = transform.parent.GetComponent<Rigidbody>();
+			    parentRigidbody.MovePosition(parentRigidbody.position + new Vector3(0, closestObject.bounds.center.y * closestObject.transform.localScale.y * 0.5f, 0));
+			    CurrentVisual = Instantiate(
+					closestObject.gameObject,
+					transform.position,
+					transform.rotation,
+					transform.parent);
+			    CurrentVisual.transform.localScale = closestObject.transform.localScale;
+			    CurrentVisual.GetComponent<MeshCollider>().convex = true;
+			    var existingRigidbody = CurrentVisual.GetComponent<Rigidbody>();
+
+				if(existingRigidbody != null)
+					Destroy(existingRigidbody);
+		    }
+	    }
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		_closeObjects.Add(other);
+	}
+
+	public void OnTriggerExit(Collider other)
+	{
+		_closeObjects.Remove(other);
+	}
+}
